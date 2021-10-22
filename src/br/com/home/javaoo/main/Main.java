5e8f56ed18b299fcf1deff4a4cfac19401eb5eae
RodResq <rodrigoresque@yahoo.com.br>
2021-10-22 18:05:58 -0300

    updt

--- src/br/com/home/javaoo/main/Main.java
+++ src/br/com/home/javaoo/main/Main.java
@@ -1,8 +1,18 @@
 package br.com.home.javaoo.main;
 
+import br.com.home.javaoo.classes.Veiculo;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ola mundo!");
+        Veiculo corsa = new Veiculo();
+        corsa.marca = "GM";
+        corsa.nome = "Corsa";
+        corsa.ligar();
+
+        Veiculo celta = new Veiculo();
+        celta.marca = "GM";
+        celta.nome = "celta";
+        celta.ligar();
     }
 }
